9589a057b022bd9b2a77db34231d960e19d69b42
kishida01 <143906056+kishida01@users.noreply.github.com>
2023-10-09 18:32:56 +0900

    kadai_mdn_js upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,10 @@
+const getTodayDate = () => {
+    const today = new Date();
+    let year = today.getFullYear();
+    let month = today.getMonth() + 1;
+    let day = today.getDate();
+
+    console.log(year + '年' + month + '月' + day + '日');
+}
+
+getTodayDate();
