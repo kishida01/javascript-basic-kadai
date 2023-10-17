cacc49475d778ea8ebe0b37ee8a4b135e9363337
kishida01 <143906056+kishida01@users.noreply.github.com>
2023-10-17 13:36:51 +0900

    kadai_20 upload

---
+++ kadai_20/event.js
@@ -0,0 +1,6 @@
+const text = document.getElementById('text')
+const botton = document.getElementById('btn'); 
+
+botton.addEventListener('click' , () => {
+    text.textContent = 'ボタンをクリックしました' ;
+});
